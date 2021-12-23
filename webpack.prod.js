6ad2e53e3e852abae1cb1a0cf5fbb134c0f8f315
Kévin Olivar <kevin.olivar@valtech.com>
2021-12-23 23:12:17 +0100

    first commit

---
+++ webpack.prod.js
@@ -0,0 +1,51 @@
+const { merge }               = require('webpack-merge');
+const CssMinimizerPlugin      = require("css-minimizer-webpack-plugin");
+const TerserPlugin            = require('terser-webpack-plugin');
+const common                  = require('./webpack.common.js');
+
+module.exports = merge(common, {
+    mode: 'production',
+    optimization: {
+        minimize: true,
+        minimizer: [
+            new TerserPlugin({}),
+            new CssMinimizerPlugin({
+                minimizerOptions: {
+                preset: [
+                    "default",
+                    {
+                    calc: true,
+                    convertValues: true,
+                    discardComments: {
+                        removeAll: true,
+                    },
+                    discardDuplicates: true,
+                    discardEmpty: true,
+                    mergeRules: true,
+                    normalizeCharset: true,
+                    reduceInitial: true, // This is since IE11 does not support the value Initial
+                    svgo: true,
+                    canPrint: false,
+                    },
+                ],
+                },
+                minify: [
+                    CssMinimizerPlugin.cssnanoMinify,
+                    CssMinimizerPlugin.cleanCssMinify,
+                ],
+            }),
+        ],
+        splitChunks: {
+            cacheGroups: {
+                main: {
+                    chunks: 'all',
+                    name: 'site',
+                    test: 'main',
+                    enforce: true
+                }
+            }
+        }
+    },
+    devtool: false,
+    performance: { hints: false }
+});
